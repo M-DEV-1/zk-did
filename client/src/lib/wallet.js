fbd36e91cac30d1380fa612ac69e1efb8fcfc42c
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-06-23 03:29:55 +0530

    add: wallet connection logic
    Signed-off-by: M-DEV-1 <mahadevankizhakkedathu@gmail.com>

---
+++ client/src/lib/wallet.js
@@ -0,0 +1,14 @@
+// lib/wallet.js
+
+import { createConfig, http } from "wagmi";
+import { metaMask } from "wagmi/connectors";
+import { sepolia } from "wagmi/chains";
+
+export const config = createConfig({
+  appName: "ZK-DID",
+  chains: [sepolia],
+  connectors: [metaMask()],
+  transports: {
+    [sepolia.id]: http()
+  },
+});
