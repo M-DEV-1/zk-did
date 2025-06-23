3bc7e7d636d7545df657b8a00ee41ffad9a84d45
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-06-23 23:51:36 +0530

    add: shadcn toast, useToast, toaster, alert, card
    Signed-off-by: M-DEV-1 <mahadevankizhakkedathu@gmail.com>

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import WagmiWrapper from "@/components/wagmiWrapper";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         <WagmiWrapper>{children}</WagmiWrapper>
       </body>
     </html>
