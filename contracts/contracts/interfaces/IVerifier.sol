a9b65c37ca0f7d5e9e5eb9fcbee6f0506cf73a01
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-07-11 19:26:02 +0530

    add: contracts for script verification
    Signed-off-by: M-DEV-1 <mahadevankizhakkedathu@gmail.com>

--- contracts/contracts/interfaces/IVerifier.sol
+++ contracts/contracts/interfaces/IVerifier.sol
@@ -6,6 +6,6 @@ interface IVerifier {
         uint[2] memory a,
         uint[2][2] memory b,
         uint[2] memory c,
-        uint[1] memory input
+        uint[] memory input
     ) external view returns (bool r);
 }
